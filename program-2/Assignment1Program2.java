37b689bcf51c4494babf0d89de41b96fd60654e8
ronald-clayton <ronaldclayton3rd@gmail.com>
2025-09-18 01:34:29 -0400

    Updated Assignment 1 program 2

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Assignment1Program2 {
-  public static void main(String[] args) {
-
-  }
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter a base (2-36)");
+int base = scanner.nextInt();
+int largest4Digit = (int) Math.pow(base, 4) - 1;
+System.out.println("the maximum, 4-digit, base 10 number in base " + base + " is " + largest4Digit);
+System.out.print("now, enter a base 10 number in range 0 to" + largest4Digit + "to convert:");
+int number = scanner.nextInt();
+String converted = Integer.toString(number,base);
+System.out.println(number + "(base 10)=" + converted + "(base)" + base);
+scanner.close();
+}
 }
